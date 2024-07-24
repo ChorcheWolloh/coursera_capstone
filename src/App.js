359f9d708f9bb81f1c2dfb9d49b6db3cc51f8f92
Georgijs V. <4hollow22@gmail.com>
2024-07-24 11:02:30 +0300

    Added semantic HTML and refactored overall structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Main from './components/Main';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (

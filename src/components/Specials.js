8b7fe7d2c772df41b8259d6da4cf57e6946163c7
Georgijs V. <4hollow22@gmail.com>
2024-07-29 13:30:38 +0300

    Added basic states to specials and testimon cards

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Card() {
+function Card({props}) {
     return (
             <div className='productItem'>
                 <img src='/placeholder.png' alt=''/>
                 <div className='cardTopRow'>
-                    <h4>Title</h4>
-                    <h4>$Price</h4>
+                    <h4>{props.title}</h4>
+                    <h4>${props.price}</h4>
                 </div>
-                <p>
-                    Description of an item including ingredients and some tasty descriptions to get the juices flowing
-                </p>
+                <p>{props.description}</p>
                 <div className='cardFooter'>
                     <span>Order a Delivery</span>
                     <img src='\delivery.svg' alt='moped icon'/>
@@ -20,6 +18,13 @@ function Card() {
   }
 
 const Specials = () => {
+
+    const [specials, setSpecials] = useState([
+        {title: 'Greek salad', price: '12.99', description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'},
+        {title: 'Bruchetta', price: '5.99', description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.'},
+        {title: 'Lemon Dessert', price: '5.00', description: 'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.'}
+    ])
+
   return (
     <article id='specials'>
         <div id='SpecialsTopRow'>
@@ -27,9 +32,9 @@ const Specials = () => {
             <button>Online Menu</button>
         </div>
         <div className='cardContainer'>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card props={specials[0]}/>
+            <Card props={specials[1]}/>
+            <Card props={specials[2]}/>
         </div>
     </article>
   )
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TestimonItem = () => {
+const TestimonItem = ({props}) => {
     return (
         <div className='testimonItem'>
-            <h4>Rating</h4>
+            <h4>{props.rating}</h4>
             <div class='itemInsideContainer'>
-                <img src='/placeholder.png' alt='avatar'/>
-                <p>Name</p>
+                <img src={props.image} alt='avatar'/>
+                <p>{props.name}</p>
             </div>
-            <p>Review text</p>
+            <p>{props.review}</p>
         </div>
     )
 }
 
 const Testimonials = () => {
+
+    const [testimon, setTestimon] = useState([
+        {name:'Bob', rating:'5.0', review:'Everything was great!', image:'/placeholder.png'},
+        {name:'Alice', rating:'4.7', review:'The food is delcious.', image:'/placeholder.png'},
+        {name:'Mary', rating:'4.8', review:'Recommend to everyone!', image:'/placeholder.png'},
+        {name:'Stewen', rating:'5.0', review:'Amazing service', image:'/placeholder.png'}
+    ])
+
   return (
     <article id='testimonials'>
         <h2>Testimonials</h2>
         <div className='testimonContainer'>
-            <TestimonItem/>
-            <TestimonItem/>
-            <TestimonItem/>
-            <TestimonItem/>
+            <TestimonItem props={testimon[0]} />
+            <TestimonItem props={testimon[1]} />
+            <TestimonItem props={testimon[2]} />
+            <TestimonItem props={testimon[3]} />
         </div>
     </article>
   )
